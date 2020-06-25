5a70a6ccafac660bec27d5f741327b423325b605
jatibansaljb2001 <62650742+jatibansaljb2001@users.noreply.github.com>
2020-06-25 11:18:03 +0530

    ALL fixed 2

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:80')
+const socket = io.connect('http://localhost')
 
 const form = document.getElementById('send-container')
 const messageInput = document.getElementById('messageInp')
