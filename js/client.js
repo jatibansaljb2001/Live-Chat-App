0665de4c0a84993588e30e0d597095c15b48cd60
jatibansaljb2001 <62650742+jatibansaljb2001@users.noreply.github.com>
2020-06-25 08:28:42 +0530

    Added Live chat app v1

---
+++ js/client.js
@@ -0,0 +1,41 @@
+const socket = io('http://localhost:8000')
+
+const form = document.getElementById('send-container')
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".container")
+var audio = new Audio('allmessage.mp3')
+
+const append = (message, position1, position2) => {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageElement.classList.add('message')
+    messageElement.classList.add(position1)
+    messageElement.classList.add(position2)
+    messageContainer.append(messageElement)
+    if (position1 === 'left') {
+        audio.play()
+    }
+}
+
+const name = prompt("Enter Your Name To Join: ")
+socket.emit('new-user-joined', name)
+
+socket.on('user-joined', data => {
+    append(`${data} joined the chat`, 'left', 'join')
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const message = messageInput.value
+    append(`You: ${message}`, 'right')
+    socket.emit('send', message)
+    messageInput.value = ''
+})
+
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left')
+})
+
+socket.on('left', data => {
+    append(`${data} left the chat`, 'left', 'leave')
+})
